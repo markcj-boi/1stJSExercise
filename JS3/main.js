4e5ad07bd7d1ef2c5d8b1d6b3b44cbd66c4c07cb
markcj-boi <markcjbalatayo@gmail.com>
2025-03-21 09:48:06 +0800

    1stJSAct

--- JS3/main.js
+++ JS3/main.js
@@ -1,4 +1,53 @@
 const elementTxt = document.getElementById("elements");
 let numInpt = document.getElementById("numInpt");
 const sumTxt = document.getElementById("sum");
-const highNum = document
+const highNum = document.getElementById("highNum");
+const lownum = document.getElementById("lowNum");
+let sum = 0;
+let largest = 0;
+
+let arr =  [];
+let smallest=0;
+let arrLength = arr.length;
+
+
+function insertEl() {
+    arr.push(parseInt(numInpt.value));
+    smallest = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] < smallest){
+            smallest = arr[i];
+        }
+        if(arr[i] > largest){
+            largest = arr[i];
+        }
+    }
+
+    elementTxt.innerHTML = arr.join("<br>");
+    sumTxt.innerHTML = sum;
+    highNum.innerHTML = largest;
+    lowNum.innerHTML = smallest;
+    console.log(arr);
+
+}
+
+
+function deleteAll(){
+
+    arr = [];
+    
+    // while(arrLength < 0){
+    //     arr.pop();
+    // }
+    sum = 0;
+    largest = 0;
+    elementTxt.innerHTML = arr.join("<br>");
+    sumTxt.innerHTML = "";
+    highNum.innerHTML = "";
+    lowNum.innerHTML = "";
+    console.log(arr);
+}
